4332c4c54a8450e086086b4b41c903b8f042ea2c
Jsvi53 <2101203142@QQ.COM>
2025-03-01 09:12:14 +0800

    new file:   .gitignore 	new file:   CMakeLists.txt 	new file:   README.md 	new file:   include/mainwindow.h 	new file:   src/main.cpp 	new file:   src/mainwindow.cpp 	new file:   src/mainwindow.ui 	new file:   src/qt_custom_widget_zh_CN.ts

---
+++ src/qt_custom_widget_zh_CN.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="qt_custom_widget_zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="../build/qt_custom_widget_autogen/include/ui_mainwindow.h" line="50"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
